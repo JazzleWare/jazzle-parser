45040264e6574babad61118f0d368ce89087ab8b
zenekron <zenekron@gmail.com>
2016-07-10 12:36:28 +0200

    Add standalone module to parse the spread operator

---
+++ src/parser/parse/spread.js
@@ -0,0 +1,18 @@
+/* global PREC_WITH_NO_OP, CONTEXT_NONE */
+
+function parseSpread() {
+  var startc = this.c - 1 - 2; // FIXME: -3?
+  var startLoc = this.locOn(1 + 2);
+  this.next ();
+
+  var e = this.parseNonSeqExpr(PREC_WITH_NO_OP, CONTEXT_NONE);
+
+  return {
+    type: 'SpreadElement',
+    loc: { start: startLoc, end: e.loc.end },
+    start: startc, end: e.end,
+    argument: core(e) // FIXME: this.core?
+  };
+}
+
+module.exports.default = module.exports = parseSpread;
