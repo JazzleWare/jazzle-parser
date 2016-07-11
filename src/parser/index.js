cac4c138014d56f61ee611e66ca865dc238c9c73
Neni Onoda <coranach@yahoo.ie>
2016-07-11 09:18:04 -0400

    browserify bundle for inspection purposes

--- src/parser/index.js
+++ src/parser/index.js
@@ -1,15 +1,50 @@
 
 var Parser = require('./constructor.js');
 
+function mixin(mainClass, sub) {
+  Object.keys(sub).forEach(function (key) {
+     mainClass [key] = sub[key];
+  });
+}
+
 // context is autobound
 Parser.prototype.assert = require('./util/assert.js');
 
-['array', 'arrow', 'assignment', 'class', 'comment', 'esc-general', 'esc-unicode', 'export', 'for', 'fundef', 'identifier', 'idStatementOrId', 'import', 'let', 'loc', 'memname', 'new', 'nextlookahead', 'non-assig', 'number', 'obj-class-common', 'obj', 'pattern', 'primary', 'program', 'regex', 'semi', 'spread', 'statement', 'string', 'super', 'template', 'validate', 'var', 'yield'].forEach(function (uname) {
-  var component = require('./prototype/' + uname + '.js');
-  Object.keys(component).forEach(function (key) {
-    Parser.prototype[key] = component[key];
-  });
-});
-
+mixin(Parser.prototype, require('./prototype/array') );
+mixin(Parser.prototype, require('./prototype/arrow') );
+mixin(Parser.prototype, require('./prototype/assignment') );
+mixin(Parser.prototype, require('./prototype/class') );
+mixin(Parser.prototype, require('./prototype/comment') );
+mixin(Parser.prototype, require('./prototype/esc-general') );
+mixin(Parser.prototype, require('./prototype/esc-unicode') );
+mixin(Parser.prototype, require('./prototype/export') );
+mixin(Parser.prototype, require('./prototype/for') );
+mixin(Parser.prototype, require('./prototype/fundef') );
+mixin(Parser.prototype, require('./prototype/identifier') );
+mixin(Parser.prototype, require('./prototype/idStatementOrId') );
+mixin(Parser.prototype, require('./prototype/import') );
+mixin(Parser.prototype, require('./prototype/let') );
+mixin(Parser.prototype, require('./prototype/loc') );
+mixin(Parser.prototype, require('./prototype/memname') );
+mixin(Parser.prototype, require('./prototype/new') );
+mixin(Parser.prototype, require('./prototype/nextlookahead') );
+mixin(Parser.prototype, require('./prototype/non-assig') );
+mixin(Parser.prototype, require('./prototype/number') );
+mixin(Parser.prototype, require('./prototype/obj-class-common') );
+mixin(Parser.prototype, require('./prototype/obj') );
+mixin(Parser.prototype, require('./prototype/pattern') );
+mixin(Parser.prototype, require('./prototype/primary') );
+mixin(Parser.prototype, require('./prototype/program') );
+mixin(Parser.prototype, require('./prototype/regex') );
+mixin(Parser.prototype, require('./prototype/semi') );
+mixin(Parser.prototype, require('./prototype/spread') );
+mixin(Parser.prototype, require('./prototype/statement') );
+mixin(Parser.prototype, require('./prototype/string') );
+mixin(Parser.prototype, require('./prototype/super') );
+mixin(Parser.prototype, require('./prototype/template') );
+mixin(Parser.prototype, require('./prototype/validate') );
+mixin(Parser.prototype, require('./prototype/var') );
+mixin(Parser.prototype, require('./prototype/yield') );
 
 module.exports = Parser;
+
