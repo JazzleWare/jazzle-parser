024022084d2d81edf09a064a2ab59b76f6b510c9
Neni Onoda <coranach@yahoo.ie>
2016-12-09 10:34:22 -0500

    fix the case where an arrow function's rest has an argument other than an Identifier

--- src/arrow@Parser.js
+++ src/arrow@Parser.js
@@ -118,11 +118,15 @@ this. asArrowFuncArg = function(arg) {
 
         case 'SpreadElement':
             this.assert(arg !== this.firstNonTailRest);
+            if (arg.argument.type !== 'Identifier')
+              this.err('binding.rest.arg.not.id');
             this.asArrowFuncArg(arg.argument);
             arg.type = 'RestElement';
             return;
 
         case 'RestElement':
+            if (arg.argument.type !== 'Identifier')
+              this.err('binding.rest.arg.not.id');
             this.asArrowFuncArg(arg.argument);
             return;
 
