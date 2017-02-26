acd1c4b1f4042a4a46c045ae676e2ebd8ef91553
Neni Onoda <icefapper@gmail.com>
2017-02-26 04:48:42 -0500

    conditional expression

--- dist/jazzle.js
+++ dist/jazzle.js
@@ -1774,6 +1774,27 @@ function(n, flags, ri) {
 
 },
 function(){
+Emitters['ConditionalExpression'] = function(n, prec, flags) {
+  this.emitCondTest(n.test, flags);
+  this.wm(' ','?',' ').eN(n.consequent, PREC_NONE, EC_NONE);
+  this.wm(' ',':',' ').eN(n.alternate, PREC_NONE, EC_NONE);
+};
+
+this.emitCondTest = function(n, prec, flags) {
+  var paren = false;
+  switch (n.type) {
+  case 'AssignmentExpression':
+  case 'ConditionalExpression':
+    paren = true;
+  }
+
+  if (paren) { this.w('('); flags = EC_NONE; }
+  this.eN(n, PREC_NONE, flags);
+  if (paren) this.w(')');
+};
+
+},
+function(){
 Emitters['DoWhileStatement'] = function(n, prec, flags) {
   this.w('do').emitDependentStmt(n.body);
   if (n.body.type !== 'BlockStatement')
